41ab6721ce32fec7b7bb439b02f008e63b5c19ac
Michael Michelotti <michael.michelotti4@gmail.com>
2021-03-30 22:03:02 -0700

    Started implementation for individual card class

--- cards/card.py
+++ cards/card.py
@@ -1,3 +1,59 @@
 class Card:
-    pass
+    # Class attributes which define what ranks and suits are viable for a card
+    ranks_to_values = {str(num): num for num in range(1, 11)}
+    ranks_to_values.update(
+        {
+            'J': 10,
+            'Q': 10,
+            'K': 10,
+            'A': 11
+        }
+    )
 
+    valid_ranks = [str(num) for num in range(1, 11)]
+    valid_ranks.extend(['J', 'Q', 'K', 'A'])
+    valid_suits = ['H', 'D', 'C', 'S']
+
+    # Beginning of instance attributes
+    def __init__(self, rank, suit):
+        self._rank = rank
+        self._suit = suit
+        self.value = type(self).ranks_to_values[rank]
+
+    @property
+    def rank(self):
+        return self._rank
+
+    @rank.setter
+    def rank(self, value):
+        if not isinstance(value, str):
+            raise TypeError(f'Rank must be a string in: {", ".join(type(self).valid_ranks)}')
+        if value not in type(self).valid_ranks:
+            raise ValueError(f'Invalid rank: {value}. Rank must be in: {", ".join(type(self).valid_ranks)}')
+        self._rank = value
+
+    @property
+    def suit(self):
+        return self._rank
+
+    @suit.setter
+    def suit(self, value):
+        if not isinstance(value, str):
+            raise TypeError(f'Suit must be a string in: {", ".join(type(self).valid_suits)}')
+        if value not in type(self).valid_suits:
+            raise ValueError(f'Invalid suit: {value}.Suit must be in: {", ".join(type(self).valid_suits)}')
+        self._suit = value
+
+    def render(self):
+        return f"""
+        ________________
+        | {self.rank}    {self.rank}  |
+        |                        |
+        |                        |
+        |                        |
+        |        {self.suit}        |
+        |                        |
+        |                        |
+        | {self.rank}    {self.rank}    |
+        _________________
+        """
--- tests/test_cards/test_card.py
+++ tests/test_cards/test_card.py
@@ -1,17 +1,27 @@
 import pytest
 
-from cards import Card
+from cards.card import Card
 
 
 @pytest.fixture
-def card():
-    return Card('A', 'D')
+def test_rank():
+    return 'A'
+
+
+@pytest.fixture
+def test_suit():
+    return 'D'
+
+
+@pytest.fixture
+def card(test_rank, test_suit):
+    return Card(test_rank, test_suit)
 
 
 @pytest.fixture
 def ranks_to_values():
     non_face_cards = {str(num): num for num in range(1, 11)}
-    return non_face_cards.update(
+    non_face_cards.update(
         {
             'J': 10,
             'Q': 10,
@@ -19,6 +29,7 @@ def ranks_to_values():
             'A': 11
         }
     )
+    return non_face_cards
 
 
 @pytest.mark.parametrize(
@@ -33,3 +44,46 @@ def test_card_init(ranks_to_values, rank, suit):
     my_card = Card(rank, suit)
     assert my_card.rank == rank
     assert my_card.suit == suit
+    assert my_card.value == ranks_to_values[my_card.rank]
+
+
+@pytest.mark.parametrize(
+    ('rank', 'suit'),
+    (
+        ('Invalid', 'D'),
+        ('D', 'Q'),
+        ('7', 'Invalid')
+    )
+)
+def test_card_init_value_errors(rank, suit):
+    with pytest.raises(ValueError):
+        Card(rank, suit)
+
+
+@pytest.mark.parametrize(
+    ('rank', 'suit'),
+    (
+        (7, 'D'),
+        ('A', 10),
+        ('7', ['D', 'H'])
+    )
+)
+def test_card_init_type_errors(rank, suit):
+    with pytest.raises(TypeError):
+        Card(rank, suit)
+
+
+def test_card_render(card):
+    assert isinstance(card.render())
+
+
+def test_card_suit(card, test_suit):
+    assert card.suit == test_suit
+
+
+def test_card_rank(card, test_rank):
+    assert card.rank == test_rank
+
+
+def test_card_value(card, ranks_to_values, test_rank):
+    assert card.value == ranks_to_values[test_rank]
